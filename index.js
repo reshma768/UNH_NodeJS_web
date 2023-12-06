034b6de6bde4db56b8f3382d72ab32afc637effe
reshma768 <144494726+reshma768@users.noreply.github.com>
2023-12-06 10:21:02 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const http = require('http'); 
+const fs = require('fs');
+const path = require('path');
+http.createServer((req, res)=>{
+    console.log(req.url);
+    if(req.url ==='/'){ 
+
+       fs.readFile(path.join(__dirname,'public','index.html'),(err,content)=>{
+            if (err) throw err ;
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.end(content)
+       })
+
+    }else if(req.url ==='/styles.css'){ 
+
+        fs.readFile(path.join(__dirname,'public','styles.css'),(err,content)=>{
+            if (err) throw err ;
+            res.writeHead(200, {'Content-Type': 'text/css'})
+            res.end(content)
+        })
+
+    }else if(req.url ==='/api'){
+
+        fs.readFile(path.join(__dirname,'public','db.json'),(err,content)=>{
+            if (err) throw err ;
+            res.writeHead(200, {'Content-Type': 'application/json'})
+            res.end(content)
+        })
+
+    }else{
+
+        res.end("<h1>404 Nothing is here</h1>")
+
+    }
+
+}).listen(5959,()=>console.log("Server is running on port 5959"));
